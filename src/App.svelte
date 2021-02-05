759d7e0d0b25aaf3360cb46a6ce38a2ab2cb5fbb
stevecode <suarezsteve21@gmail.com>
2021-02-04 20:57:57 -0500

    Styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,54 @@
 <script>
-	export let name;
+  export let name, lastName;
+  // Para todo uso let, en este caso para guardar un string
+  let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name} {lastName}!</h1>
+  <p>Frontend Developer</p>
+  <!-- Añado una imagen -->
+  <img src={svelteLogo} alt="Svelte" />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  /* Este estilo aplicará en toda la app */
+  :global(body) {
+    background-color: #f2eee2;
+    color: #0084f6;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  /* Usamos variables globales, para esto usamos :root */
+  :global(:root) {
+    --theme-color: purple;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  /* Establezco el color con base a la variable global, con la palabra reservada var */
+  p {
+    color: var(--theme-color);
+    font-size: 22px;
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  main img {
+    width: 30%;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
