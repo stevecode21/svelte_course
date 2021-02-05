bc8c648bf5c826536e999a8d6036c26e481120d1
stevecode <suarezsteve21@gmail.com>
2021-02-04 21:20:28 -0500

    Reactivity

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,11 +1,18 @@
 <!-- Logic -->
 <script>
   let someText = `Frontend Developer`;
+  let count = 0;
+
+  // Creo una función para manejar los clicks
+  const handleClick = () => {
+    count += 1;
+  };
 </script>
 
 <!-- Como buena práctica poner un div para envolver todo el html -->
 <div class="about">
   <p>{someText}</p>
+  <button on:click={handleClick}>Click {count === 0 ? "" : count}</button>
 </div>
 
 <!-- Styles -->
